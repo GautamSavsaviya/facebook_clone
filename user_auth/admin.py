6ffe5087e0885a9d9830616139fa2f537cb637ce
GautamSavsaviya <gautamsavasaviya222@gmail.com>
2024-01-29 22:03:10 +0530

    Customize admin pannel

--- user_auth/admin.py
+++ user_auth/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 from .models import User, Profile
 
 # Register your models here.
-admin.site.register(User)
-admin.site.register(Profile)
+
+@admin.register(User)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'full_name', 'email', 'gender']
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'full_name', 'verified']
+    list_editable = ['verified']
+
+
+# admin.site.register(User, CustomUserAdmin)
+# admin.site.register(Profile, ProfileAdmin)
